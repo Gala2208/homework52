7e9b5eb20718e7d9fb62bcd1d7455bd14de41a9d
Gala2208 <toppers.ogre-0k@icloud.com>
2022-11-22 09:49:49 +0500

    Create

---
+++ homework52/Program.cs
@@ -0,0 +1,33 @@
+﻿try
+{
+    int m = 4,
+        n = 4;
+    int[,] arr = new int[m, n];
+    int[] result = new int[m];
+    Random rnd = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            arr[i, k] = rnd.Next(1, 10);
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            result[i] += arr[i, k];
+        }
+    }
+
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    foreach (int num in result)
+    {
+        Console.Write("{0} ", num);
+    }
+} catch(Exception err)
+{
+    Console.WriteLine(err);
+}
